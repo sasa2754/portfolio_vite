0b9ed19e04c642f050d4b65bdf0b10b5d4337130
Sabrina <sasamor5@gmail.com>
2024-09-16 20:48:11 -0300

    projetos colocados

--- src/App.jsx
+++ src/App.jsx
@@ -18,6 +18,7 @@ function App() {
       <main>
         <Rect id={'SobreMim'} title={'Sobre mim'} text={'Meu nome é Sabrina, eu tenho 19 anos, comecei a estudar programação no final de 2023. Minha área favorita é programação web, mas gosto de me aventurar em várias áreas!'} photo={'/img/eu.jpg'}></Rect>
         <RectReverse id={'DrumsHero'} title={'Drums Hero'} text={'Meu primeiro projeto grandioso foi o Drums Hero, ele foi inspirado no jogo de ritmo "guitar hero", ele foi feito como projeto final da matéria de IoT na bosch, consistia em uma bateria elétrica com 6 tambores e um site para acompanhar as músicas.'} photo={'./img/drums.jpg'} link={'https://github.com/sasa2754/Drums_Hero.git'}></RectReverse>
+        <Rect id={'Kangoroo'} title={'Kangoroo'} text={'O Kangoroo é meu maior projeto até agora, ele foi desenvolvido como projeto final da matéria de web na bosch, é meu xodózinho, ele é inspirado na rede social "Instagram", fizemos ele em duas semanas, superando o previsto para o pouco tempo. Ele tem todas as funcionalidades de uma rede social, como postar, curtir, comentar e até mesmo um chat em tempo real.'} photo={'img/kangorooLogo.png'} link={'https://github.com/wallacin07/SocialMedia_WebProject.git'}></Rect>
       </main>
       
       <footer id='Contato'>
