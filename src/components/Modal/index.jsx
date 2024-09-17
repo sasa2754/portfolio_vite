156163ae28b19052916261a3771c640de3eb69eb
Sabrina <sasamor5@gmail.com>
2024-09-16 21:44:14 -0300

    transição suave do scroll e css

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -1,7 +1,6 @@
 import style from './modal.module.css'
 
 export default function Modal({visible}) {
-    console.log(visible)
     return (
         <div className={visible ? style.modal : style.vazio}>
             <a href="#SobreMim"><h3>Sobre mim</h3></a>
